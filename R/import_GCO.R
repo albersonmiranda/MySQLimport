94a5855e0f4c3db683f784d2182675083db0c978
Alberson Miranda <albersonmiranda@hotmail.com>
2021-09-10 14:37:31 -0300

    added vroom method to import_gco

--- R/import_GCO.R
+++ R/import_GCO.R
@@ -19,89 +19,172 @@
 #' @examples
 #' \dontrun{
 #' # para importar para o R:
-#' data = import_GCO("C:\\Arquivos\\GCO\\202009.txt")
+#' data <- import_gco("C:\\Arquivos\\GCO\\202009.txt")
 #'
 #' # para importar para o MySQL:
-#' import_GCO("C:\\Arquivos\\GCO\\202009.txt", MySQL = TRUE, tabela = "t202009")
+#' import_gco(
+#'   "C:\\Arquivos\\GCO\\202009.txt",
+#'   mysql = TRUE,
+#'   tabela = "t202009",
+#'   user = "alberson"
+#' )
 #' }
 #'
+import_gco = function(caminho,
+                       mysql = FALSE,
+                       schema = "GCO",
+                       tabela,
+                       metodo = c("vroom", "readr"),
+                       host,
+                       user) {
 
-
-import_GCO = function (caminho, MySQL = FALSE, schema = "GCO", tabela) {
-
-  # evaluate args
-  if (MySQL == TRUE && is.null(tabela)) {
-
+  # evaluate arg tabela
+  if (mysql == TRUE && is.null(tabela)) {
     stop("tabela deve ser informada")
-
   }
+
+  # evaluate arg host
+  if (mysql == TRUE && is.null(user)) {
+    stop("host deve ser informado")
+  }
+
+  # evaluate arg user
+  if (mysql == TRUE && is.null(user)) {
+    stop("user deve ser informado")
+  }
+
+  # evaluate arg metodo
+  match.arg(metodo)
 
   # conexao com MySQL
-  if (MySQL == TRUE) {
-
+  if (mysql == TRUE) {
     tryCatch({
-      con = DBI::dbConnect(RMySQL::MySQL(),
-                           host = "localhost",
-                           db = schema,
-                           user = "root",
-                           password = rstudioapi::askForPassword("Database password")
-      )},
-      error = function (cond) {
+        con = DBI::dbConnect(RMySQL::MySQL(),
+          host = host,
+          db = schema,
+          user = user,
+          password = rstudioapi::askForPassword("Database password")
+        )
+      },
+      error = function(cond) {
         message(cond)
-        stop(paste("Talvez voce nao tenha criado o schema", schema, "no MySQL?"))
-      })
+        stop(
+          paste("Talvez você não tenha criado o schema", schema, "no MySQL?")
+        )
+      }
+    )
   }
 
-  # importar arquivo para o R
-  data = readr::read_fwf(
-    caminho,
-    readr::fwf_widths(
-      c(6, 4, 4, 20, 8, 3, 4, 8, 8, 17, 17, 17, 4, 1, 2, 2, 2, 2, 5, 4, 14,
-        30, 13, 1, 1, 2, 8, 20, 2, 1, 1, 13, 11, 2, 2, 2, 2, 17, 9, 1, 4, 1,
-        2, 2, 2),
-      c("processamento", "produto", "subproduto", "contrato", "clientegco",
-        "sistema", "agencia", "efetivacao", "vencimento", "saldodevedor",
-        "saldocontabil", "saldopdd", "diasatraso", "adiantamento",
-        "classorigem", "class", "classinformada", "classfinal", "grupocontabil",
-        "diasprazo", "cpfcnpj", "nome", "vlrutilizadopdd", "prejuizo",
-        "tipocredito", "codinterno", "transfprejuizo", "tipoperda", "idempresa",
-        "procadm", "procjud", "saldora", "cosif", "classggc",
-        "classoperacional", "idpdd", "classrisco", "contratado", "txjuros",
-        "tipotx", "indexador", "tipocli", "canalorigem", "canalatual",
-        "classoficial")),
-    col_types = readr::cols(
-      tipocli = readr::col_character(),
-      adiantamento = readr::col_character(),
-      tipoperda = readr::col_character(),
-      procadm = readr::col_character(),
-      procjud = readr::col_character(),
-      classoperacional = readr::col_character(),
-      classoficial = readr::col_character(),
-      txjuros = readr::col_double(),
-      diasatraso = readr::col_double(),
-      diasprazo = readr::col_double(),
-      vlrutilizadopdd = readr::col_double(),
-      contrato = readr::col_character(),
-      saldodevedor = readr::col_double(),
-      saldocontabil = readr::col_double(),
-      saldora = readr::col_double(),
-      contratado = readr::col_double(),
-      saldopdd = readr::col_double(),
-      processamento = readr::col_character(),
-      vencimento = readr::col_date("%Y%m%d"),
-      transfprejuizo = readr::col_date("%Y%m%d"),
-      efetivacao = readr::col_date("%Y%m%d")))
+  # importar arquivo para o R via {readr}
+  if (metodo == "readr") {
+    data = readr::read_fwf(
+      caminho,
+      readr::fwf_widths(
+        c(
+          6, 4, 4, 20, 8, 3, 4, 8, 8, 17, 17, 17, 4, 1, 2, 2, 2, 2, 5, 4, 14,
+          30, 13, 1, 1, 2, 8, 20, 2, 1, 1, 13, 11, 2, 2, 2, 2, 17, 9, 1, 4, 1,
+          2, 2, 2
+        ),
+        c(
+          "processamento", "produto", "subproduto", "contrato", "clientegco",
+          "sistema", "agencia", "efetivacao", "vencimento", "saldodevedor",
+          "saldocontabil", "saldopdd", "diasatraso", "adiantamento",
+          "classorigem", "class", "classinformada", "classfinal",
+          "grupocontabil", "diasprazo", "cpfcnpj", "nome", "vlrutilizadopdd",
+          "prejuizo", "tipocredito", "codinterno", "transfprejuizo",
+          "tipoperda", "idempresa", "procadm", "procjud", "saldora", "cosif",
+          "classggc", "classoperacional", "idpdd", "classrisco", "contratado",
+          "txjuros", "tipotx", "indexador", "tipocli", "canalorigem",
+          "canalatual", "classoficial"
+        )
+      ),
+      col_types = readr::cols(
+        tipocli = readr::col_character(),
+        adiantamento = readr::col_character(),
+        tipoperda = readr::col_character(),
+        procadm = readr::col_character(),
+        procjud = readr::col_character(),
+        classoperacional = readr::col_character(),
+        classoficial = readr::col_character(),
+        txjuros = readr::col_double(),
+        diasatraso = readr::col_double(),
+        diasprazo = readr::col_double(),
+        vlrutilizadopdd = readr::col_double(),
+        contrato = readr::col_character(),
+        saldodevedor = readr::col_double(),
+        saldocontabil = readr::col_double(),
+        saldora = readr::col_double(),
+        contratado = readr::col_double(),
+        saldopdd = readr::col_double(),
+        processamento = readr::col_character(),
+        vencimento = readr::col_date("%Y%m%d"),
+        transfprejuizo = readr::col_date("%Y%m%d"),
+        efetivacao = readr::col_date("%Y%m%d")
+      )
+    )
+  }
+
+  # importar arquivo para o R via {vroom}
+  if (metodo == "vroom") {
+    data = vroom::vroom_fwf(
+      caminho,
+      readr::fwf_widths(
+        c(
+          6, 4, 4, 20, 8, 3, 4, 8, 8, 17, 17, 17, 4, 1, 2, 2, 2, 2, 5, 4, 14,
+          30, 13, 1, 1, 2, 8, 20, 2, 1, 1, 13, 11, 2, 2, 2, 2, 17, 9, 1, 4, 1,
+          2, 2, 2
+        ),
+        c(
+          "processamento", "produto", "subproduto", "contrato", "clientegco",
+          "sistema", "agencia", "efetivacao", "vencimento", "saldodevedor",
+          "saldocontabil", "saldopdd", "diasatraso", "adiantamento",
+          "classorigem", "class", "classinformada", "classfinal",
+          "grupocontabil", "diasprazo", "cpfcnpj", "nome", "vlrutilizadopdd",
+          "prejuizo", "tipocredito", "codinterno", "transfprejuizo",
+          "tipoperda", "idempresa", "procadm", "procjud", "saldora", "cosif",
+          "classggc", "classoperacional", "idpdd", "classrisco", "contratado",
+          "txjuros", "tipotx", "indexador", "tipocli", "canalorigem",
+          "canalatual", "classoficial"
+        )
+      ),
+      col_types = readr::cols(
+        tipocli = readr::col_character(),
+        adiantamento = readr::col_character(),
+        tipoperda = readr::col_character(),
+        procadm = readr::col_character(),
+        procjud = readr::col_character(),
+        classoperacional = readr::col_character(),
+        classoficial = readr::col_character(),
+        txjuros = readr::col_double(),
+        diasatraso = readr::col_double(),
+        diasprazo = readr::col_double(),
+        vlrutilizadopdd = readr::col_double(),
+        contrato = readr::col_character(),
+        saldodevedor = readr::col_double(),
+        saldocontabil = readr::col_double(),
+        saldora = readr::col_double(),
+        contratado = readr::col_double(),
+        saldopdd = readr::col_double(),
+        processamento = readr::col_character(),
+        vencimento = readr::col_date("%Y%m%d"),
+        transfprejuizo = readr::col_date("%Y%m%d"),
+        efetivacao = readr::col_date("%Y%m%d")
+      )
+    )
+  }
 
   # criar tabela no MySQL
+  if (mysql == TRUE) {
 
-  if (MySQL == TRUE) {
-
+    # dropar tabela
     DBI::dbSendQuery(con, paste("DROP TABLE IF EXISTS", tabela))
 
+    # gravar tabela
     DBI::dbWriteTable(con, DBI::SQL(tabela), data)
 
+    # gravar índices
     DBI::dbSendQuery(con, paste0("
-    ALTER TABLE ", schema, ".", tabela,"
+    ALTER TABLE ", schema, ".", tabela, "
     ADD INDEX idx_contrato (contrato(20)),
     ADD INDEX idx_prejuizo (prejuizo(1)),
     ADD INDEX idx_cpfcnpj (cpfcnpj(14)),
@@ -116,8 +199,6 @@ import_GCO = function (caminho, MySQL = FALSE, schema = "GCO", tabela) {
     # desconectando
     DBI::dbDisconnect(con)
   } else {
-
     data
   }
-
 }
